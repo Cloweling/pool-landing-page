bca50eef545687689716f15d5105101880b53487
Renatogranados <renatogranadosgt@gmail.com>
2022-01-03 15:27:48 -0600

    Feat: Add section intersection 1 for desktop

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
 import { Header } from '../header'
+import Intersection1 from '../Intersections/Intersection-1'
 
 import './index.css'
 
@@ -12,6 +13,7 @@ export const Layout = () => {
   return (
     <ThemeContext.Provider value={{ ...theme, setTheme }}>
       <Header />
+      <Intersection1 />
     </ThemeContext.Provider>
   )
 }
