223efd19a752c3bde03203939a2cbef63e5b1cb9
Renato Granados <67927947+Renato6GS@users.noreply.github.com>
2022-01-18 11:20:19 -0600

    Feat: Add ubication section for desktop (#5)

---
+++ src/components/ubication/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Map from '../map/index'
+import Title from '../title'
+
+import './styles.css'
+
+const DESCRIPTION = 'Alejandro Cardenas 16830 Kingsbury St APT 210 CA 91344 Granada Hills'
+
+const Ubication = () => {
+  return (
+    <section className="ubication">
+      <Title title="Ubicación" />
+      <div className="ubication--information">
+        <div className="ubication--container-description">
+          <h3 className="ubication--description">{DESCRIPTION}</h3>
+        </div>
+        <div className="ubication--container-map">
+          <Map />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Ubication
