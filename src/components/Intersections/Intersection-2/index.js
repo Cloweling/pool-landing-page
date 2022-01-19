e944dee761002b72f07c35be1e406c1c58338379
Renato Granados <67927947+Renato6GS@users.noreply.github.com>
2022-01-19 09:33:52 -0600

    Fix Change the height section (#10)
    Fix Change the height section

--- src/components/Intersections/Intersection-2/index.js
+++ src/components/Intersections/Intersection-2/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import intersection2 from '../../../images/intersection-2/intersection-2.jpg'
+import './styles.css'
 
 export default function Intersection2() {
   return (
-    <div className="inteserction2--container">
-      <img src={intersection2} alt="Pitu's Poolman Intersecion-2" className="logo" />
+    <div className="intersection2--container">
+      <img src={intersection2} alt="Pitu's Poolman Intersecion-2" className="intersection2--image" />
     </div>
   )
 }
